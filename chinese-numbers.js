a4196ed2e3e52444c1864d6d60e5a0acbe716fa6
Anton Ivanov <anton@imago.ai>
2017-09-13 19:52:52 +0800

    add comment to new method

--- chinese-numbers.js
+++ chinese-numbers.js
@@ -165,13 +165,21 @@ ChineseNumber.prototype.toInteger = function () {
 };
 
 /**
- *
+ * Checks whether the character is part of a number (either a number itself, or
+ * a space/comma), or a part of unrelated text.
+ * @param {string} character - A string containing only one character to be
+ *    checked.
  * @returns {boolean} True if the `character` is a Chinese or Arabic number
  *    or a character like "comma" and "space", which should not delimit two 
  *    numbers; rather, they mean that the number may continue. E.g. "6,000" or
  *    "6 000".
  */
 ChineseNumber.isNumberOrSpace = function (character) {
+  // Make sure `character`.length is exactly 1:
+  if (character === null || character === undefined || character.length !== 1) {
+    throw 'isNumberOrSpace() must receive exactly one character for checking.';
+  }
+
   // Check for Arabic numbers, commas and spaces:
   if (character.match(/[0-9,\s]/)) {
     return true;
