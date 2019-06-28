927573d2f700061aa646f9e47d997d22490949f4
Anton Ivanov <anton.ivanov@prenetics.com>
2019-06-28 10:08:29 +0800

    minor change in addMissingUnits

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { SUPPORTED_CHINESE_NUMBERS } from './types/SupportedChineseNumber';
+import { CHINESE_TO_ARABIC } from './constants/ChineseToArabic';
+import { REVERSE_MULTIPLIERS } from './constants/ReverseMultipliers';
 
+/**
+ * Converter for Chinese numbers like 1000萬.
+ * @param {string} source - The original number string, e.g. 1000萬.
+ * @returns {ChineseNumber}
+ * @constructor
+ */
 export class ChineseNumber {
   sourceString: string;
 
@@ -15,43 +22,60 @@ export class ChineseNumber {
     return 'TODO FIXME';
   }
 
-  private addMissingUnits(str: string): string {
-    return 'TODO FIXME';
-  }
-
-  private isArabicNumber(character: string): boolean {
-    return true; // TODO FIXME
-  }
-
-  private isChineseNumber(character: string): boolean {
-    return true; // TODO FIXME
-  }
-
-  private isCommaOrSpace(character: string): boolean {
-    return true; // TODO FIXME
-  }
-
   /**
-   * Checks whether the character is part of a number (either a number itself, or
-   * a space/comma), or a part of unrelated text.
-   * @param {string} character - A string containing only one character to be
-   *    checked.
-   * @returns {boolean} True if the `character` is a Chinese or Arabic number
-   *    or a character like "comma" and "space", which should not delimit two
-   *    numbers; rather, they mean that the number may continue. E.g. "6,000" or
-   *    "6 000".
+   * Converts a string like 8千3萬 into 8千3百萬 (8300*10000).
+   * @param {string} str - The original string.
+   * @returns {string} The converted, expanded string.
    */
-  private isNumberOrSpace(character: string): boolean {
-    // Make sure `character`.length is exactly 1:
-    if (character === null || character === undefined || character.length !== 1) {
-      throw new Error('isNumberOrSpace() must receive exactly one character for checking.');
-    }
+  private addMissingUnits(str: string): string {
+    const characters = str.split('');
+    let result = '';
+    const numbers = CHINESE_TO_ARABIC;
+    const reverse = REVERSE_MULTIPLIERS;
+  
+    characters.forEach(function (character, i) {
+      // For the first character, we don't have a previous character yet, so
+      // just skip it:
+      if (i === 0) {
+        result += character;
+        return;
+      }
 
-    // Check for Arabic numbers, commas and spaces; then check if the
-    // character is on the list of Chinese number characters:
-    return character.match(/[0-9,.\s]/) || SUPPORTED_CHINESE_NUMBERS.includes(character)
-        ? true
-        : false;
+      const arabic = isNaN(Number(character))
+        ? numbers[character]
+        : parseInt(character); // if it's already arabic, just use the arabic number
+      const previousNumber: string | number = numbers[characters[i - 1]] || characters[i - 1];
+      const previousCharacterAsMultiplier: string | undefined = reverse[previousNumber.toString().replace('*', '')]
+        ? previousNumber.toString().replace('*', '')
+        : undefined;
+      const nextCharacterArabic = (numbers[characters[i + 1]] || 0).toString().replace('*', '');
+
+      if (
+        // not a multiplier like '*100':
+        typeof arabic === 'number' &&
+
+        // in the 1-9 range:
+        arabic > 0 && arabic < 10 &&
+
+        // previous character is 10, 100, 1000 or 10000:
+        previousCharacterAsMultiplier !== undefined &&
+
+        // e.g. 1000 < 10000 for 8千3萬, or it's the last character in string:
+        (parseInt(previousCharacterAsMultiplier) < parseInt(nextCharacterArabic) || characters[i + 1] === undefined) &&
+
+        // For numbers like 十五, there are no other units to be appended at the end
+        previousCharacterAsMultiplier !== '10'
+      ) {
+        // E.g. for 8千3, add 百:
+        const oneOrderSmaller = (parseInt(previousCharacterAsMultiplier) / 10).toString();
+        const missingMultiplier = reverse[oneOrderSmaller];
+        result += character + missingMultiplier;
+      } else {
+        result += character;
+      }
+    });
+  
+    return result;
   }
 
   private sourceStringEndsWithAfterManNumber(str: string): string | undefined {
